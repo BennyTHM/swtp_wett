4f205f69ffeb2df882afe976e52ecdb02ef8c564
PeterTHM <sinthoras85@web.de>
2013-12-05 17:01:41 +0100

    Pagedesign die 2te

---
+++ soccerPortal/index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Soccer Portal</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link rel="SHORTCUT ICON" href="sp.ico" type="image/x-icon">
+
+</head>
+
+<body>
+	<div id="all">
+    
+    	<div id="header">
+			<div id="logo"></div>
+			<div id="bannerBtn" class="registerBtn"></div>
+			<div id="loginH">
+				<form action="login.php" method="post">
+					<input class="formfield inset" type="text" name="e-mail" value="E-Mail">
+					<input class="formfield inset" type="text" name="passwort" value="Passwort">
+					<input type="submit" value="login">
+				</form>
+			</div> <!-- close loginH -->
+        </div> <!-- close header -->
+        
+        <div id="banner">
+        	<div id="bannerPICTURE">
+            	<div id="infoBox" class="boxText">
+					<p>ore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+				</div>
+            </div> <!-- close bannerPICTURE -->
+        </div> <!-- close banner -->
+        
+        <div id="content">
+        
+        	<div class="competBox">
+                 <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft" style="background-image:url(images/site/matchlogos/bayernM.png)
+							;">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo">
+										<div class="matchmembl">Heimspiel</div>
+										<div class="matchtime">02.12.2013 14:00</div>
+										<div class="matchmembl matchmembr">Gast</div>
+									 </div>
+                                     <div class="competitorSel bgGradient"><div class="compTextL">1FC Bayern München</div><div class="compquote">Sieg 3.20</div></div>
+                                     <div class="drwSel bgGradient"><div class="compTextL compdrwText">unentschieden</div><div class="compquote compquoteM">1.0</div></div>
+                                     <div class="competitorSel bgGradient"><div class="compTextL compTextR">BVB</div><div class="compquote compquoteR">Sieg 3.20</div></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght" style="background-image:url(images/site/matchlogos/bvb.png)
+							;">
+                            </div>
+                         </div>
+				 </div> <!-- close match -->
+			  
+              <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo"></div>
+                                     <div class="competitorSel"></div>
+                                     <div class="drwSel"></div>
+                                     <div class="competitorSel"></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght">
+                            </div>
+                         </div>
+              </div> <!-- close match -->
+			  
+			     <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo"></div>
+                                     <div class="competitorSel"></div>
+                                     <div class="drwSel"></div>
+                                     <div class="competitorSel"></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght">
+                            </div>
+                         </div>
+              </div> <!-- close match -->
+			  
+			     <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo"></div>
+                                     <div class="competitorSel"></div>
+                                     <div class="drwSel"></div>
+                                     <div class="competitorSel"></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght">
+                            </div>
+                         </div>
+              </div> <!-- close match -->
+			  
+			     <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo"></div>
+                                     <div class="competitorSel"></div>
+                                     <div class="drwSel"></div>
+                                     <div class="competitorSel"></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght">
+                            </div>
+                         </div>
+              </div> <!-- close match -->
+			  
+			     <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo"></div>
+                                     <div class="competitorSel"></div>
+                                     <div class="drwSel"></div>
+                                     <div class="competitorSel"></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght">
+                            </div>
+                         </div>
+              </div> <!-- close match -->
+			  
+			     <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo"></div>
+                                     <div class="competitorSel"></div>
+                                     <div class="drwSel"></div>
+                                     <div class="competitorSel"></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght">
+                            </div>
+                         </div>
+              </div> <!-- close match -->
+			  
+			     <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo"></div>
+                                     <div class="competitorSel"></div>
+                                     <div class="drwSel"></div>
+                                     <div class="competitorSel"></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght">
+                            </div>
+                         </div>
+              </div> <!-- close match -->
+			  
+			     <div class="match">
+                         <div class="compFBLft">
+                            <div class="compFlgLft">
+                            </div> <!-- close compFlgLft -->
+                         </div> <!-- close compFBLft -->
+                             <div class="matchoptions">
+                                     <div class="matchinfo"></div>
+                                     <div class="competitorSel"></div>
+                                     <div class="drwSel"></div>
+                                     <div class="competitorSel"></div>
+                             </div> <!-- close matchoptions -->
+                         <div class="compFBRght">
+                            <div class="compFlgRght">
+                            </div>
+                         </div>
+              </div> <!-- close match -->
+                
+            </div> <!-- close competBox -->
+            
+        </div> <!-- close content -->
+        
+    </div> <!-- close all -->
+</body>
+</html>
