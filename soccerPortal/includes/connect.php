294d5e331dd746557cca9f31906de42ca8c3a3c1
selcukbakkal <selcuk-bakkal@hotmail.de>
2013-12-11 18:10:58 +0100

    Create connect.php
    Dies ist die Hauptklasse für die Funktionen (u.a. Mysql)

---
+++ soccerPortal/includes/connect.php
@@ -0,0 +1,53 @@
+<?php
+
+class Mysql {
+
+	public $link;
+	public $result;
+	
+	function connect(){
+		$link = mysql_connect('localhost', 'root', '');
+		mysql_select_db('bet_system');
+		return $link;
+	}
+	
+
+	function exist($email, $username){
+		$query = "SELECT * FROM User WHERE email='{$email}' OR  username='{$username}'";
+		$result = mysql_query($query);
+		if (mysql_affected_rows() == 0){
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	function sendEmail($name, $mail, $password){
+		$nachricht = "Ihr Passwort ist: " . $password . 
+		". Nach der Änderung Ihres aktuellen Passworts, wird Ihr Benutzerkonto aktiviert.";
+		// $nachricht = wordwrap($nachricht, 70);
+		mail($mail, 'Mein Betreff', $nachricht);
+	}
+	
+	function random_pwd($length){
+		// Festlegung der verfÃ¼gbaren Buchstaben, Zahlen und Sonderzeichen
+		//$specialChars = array('!','@','#','$','%','&','*','(',')','_','-','+','=','[',']','<','>','?','/');
+		$chars = array_merge(range('a','z'), range('A','Z'), range(0,9));
+		// Einzelne Buchstaben entfernen
+		//unset($chars[array_search('i',$chars)]);
+		$chars = array_values($chars);
+		// Array mischen
+		shuffle($chars);
+		// Array beschneiden
+		$pwd = array_slice($chars,0,$length);
+		// RÃ¼ckgabewert als String
+		return implode('',$pwd);
+  	}
+  
+  	function getSaltFrom($username, $password){
+  		$gehashtes_passwort = crypt($username.$password, 'hash');
+  		return $gehashtes_passwort;
+  	}
+
+}
+?>
