9f68112d53ef7740b33e446ace9d6c4ab024f35b
LarsBecker <lars.becker90@web.de>
2014-01-14 20:28:26 +0100

    kontaktformular fertig, emailschicken muss noch

---
+++ soccerPortal/bearbeiteKontaktanfrage.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_POST["email"])){
+	echo "Danke, ihre Kontaktanfrage wird an die Admins weitergeleitet.";
+	
+	//TODO: Mails an Admins verteilen.
+}else{
+	echo "Sie sollten nicht hier sein. Bitte kehren Sie auf die Homepage zurück.";
+
+	}
+?>
