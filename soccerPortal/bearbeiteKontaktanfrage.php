e83df75e3bdee26396d5a029a57e3d2b578435e8
LarsBecker <lars.becker90@web.de>
2014-01-15 16:48:55 +0100

    kontakt emails verschicken

--- soccerPortal/bearbeiteKontaktanfrage.php
+++ soccerPortal/bearbeiteKontaktanfrage.php
@@ -1,9 +1,25 @@
 <?php
 
+include_once "includes/connect.php";
+
 if(isset($_POST["email"])){
-	echo "Danke, ihre Kontaktanfrage wird an die Admins weitergeleitet.";
 	
-	//TODO: Mails an Admins verteilen.
+	
+	$mysql = new Mysql();
+	$mysql->connect();
+	
+	 
+	$sql = "select * from user where isadmin=1";
+	$result=$mysql->mysqli->query($sql) or die ("Auslesen aus der Datenbank gescheitert.");
+	
+	$count = 0;
+	while ($row = $result->fetch_assoc()) {
+	sendEmail($row["email"],"Kontakt wegen ".$_POST["betreff"],$_POST["message"]);
+	$count=$count+1;
+	}
+	echo "Danke, ihre Kontaktanfrage wird an die Admins weitergeleitet. ";
+
+	
 }else{
 	echo "Sie sollten nicht hier sein. Bitte kehren Sie auf die Homepage zurück.";
 
