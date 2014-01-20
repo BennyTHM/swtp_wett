69b417514838c9467b6be681b5749ec30d32a4b3
BennyTHM <benjamin.schlosser@mni.thm.de>
2014-01-20 12:51:06 +0100

    Profil bearbeiten erweitert und Profil Einstellungen hinzugefuegt

--- soccerPortal/public/profilBear.php
+++ soccerPortal/public/profilBear.php
@@ -65,38 +65,63 @@ $sql = "SELECT * FROM user WHERE username = '". $_SESSION["username"] ."';";
 $result=$mysql->mysqli->query($sql) or die("Anfrage fehlgeschlagen: " . mysql_error());
 $row = $result->fetch_array();
 
-	echo 'Aktuelle Email Adresse: ' . $row["email"] . "<br>";
 ?>
-	<form action="profilBear.php?page=send1" method="post">
-		<p>
-			Neue Email eingeben: <input type="text" name="email" /><br>
-			Neue Email erneut eingeben: <input type="text" name="email" /><br>
-			Passwort eingeben: <input type="password" name="pw1" /><br>
-			<input type="submit" name="submit1" value="bestätigen" />
-		</p>
-	</form>
-<?php
-	echo '<br>Aktuelle Stadt: ' . $row["city"] . "<br>";
-?>
-	<form action="profilBear.php?page=send2" method="post">
-		<p>
-			Stadt ändern: <input type="text" name="stadt"/><br>
-			Passwort eingeben: <input type="password" name="pw2" /><br>
-			<input type="submit" name="submit2" value="bestätigen" /><br>
-		<p>
-	</form>
-<?php
-	echo '<br>Passwort ändern: '."<br>";
-?>
-	<form action="profilBear.php?page=send3" method="post">
-		<p>
-			Neues Passwort eingeben: <input type="password" name="npw1" /><br>
-			Neues Passwort erneut eingeben: <input type="password" name="npw2" /><br>
-			Altes Passwort eingeben: <input type="password" name="pw3" /><br>
-			<input type="submit" name="submit3" value="bestätigen" />
-		</p>
-	</form>
-	
+	<div id="profilAll">
+		<h1>Profil Daten &auml;ndern</h1>
+		<?php
+			echo 'Aktuelle Email Adresse: ' . $row["email"] . "<br>";
+		?>
+		<form action="profilBear.php?page=send1" method="post">
+			<p>
+				Neue Email eingeben: <input type="text" name="email" /><br>
+				Neue Email erneut eingeben: <input type="text" name="email" /><br>
+				Passwort eingeben: <input type="password" name="pw1" /><br>
+				<input type="submit" name="submit1" value="best&auml;tigen" />
+			</p>
+		</form>
+	<?php
+		echo '<br>Aktuelle Stadt: ' . $row["city"] . "<br>";
+	?>
+		<form action="profilBear.php?page=send2" method="post">
+			<p>
+				Stadt &auml;ndern: <input type="text" name="stadt"/><br>
+				Passwort eingeben: <input type="password" name="pw2" /><br>
+				<input type="submit" name="submit2" value="best&auml;tigen" /><br>
+			<p>
+		</form>
+	<?php
+		echo '<br>Passwort  &auml;ndern: '."<br>";
+	?>
+		<form action="profilBear.php?page=send3" method="post">
+			<p>
+				Neues Passwort eingeben: <input type="password" name="npw1" /><br>
+				Neues Passwort erneut eingeben: <input type="password" name="npw2" /><br>
+				Altes Passwort eingeben: <input type="password" name="pw3" /><br>
+				<input type="submit" name="submit3" value="best&auml;tigen" />
+			</p>
+		</form>
+		
+		<h1>Profil Einstellungen</h1>
+		
+		<p>Soll angezeigt werden ob sie online sind?</p>
+		<p><input type="checkbox" name="msg[1]" id="msg1393" value="1393">Ja, meinen Freundne soll angezeigt werden, dass ich online bin.</p>
+		<p><input type="checkbox" name="msg[1]" id="msg1393" value="1393">Nein, meinen Freunden soll nicht angezeigt werden, dass ich online bin</p>
+		
+		<p>Wollen Sie die Shoutbox angezeigt bekommen?</p>
+		<p><input type="checkbox" name="msg[1]" id="msg1393" value="1393">Ja, die Shoutbox soll angezeigt werden</p>
+		<p><input type="checkbox" name="msg[1]" id="msg1393" value="1393">Nein, die Shoutbox soll nicht angezeigt werden </p>
+		
+		<p>Wollen Sie per Email benachrichtigt werden, wenn Sie ein neue Nachricht im Spiel bekommen haben?</p>
+		<p><input type="checkbox" name="msg[1]" id="msg1393" value="1393">Ja, ich m&ouml;chte benachrichtigt werden</p>
+		<p><input type="checkbox" name="msg[1]" id="msg1393" value="1393">Nein, ich m&ouml;chte nicht benachrichtigt werden </p>
+		
+		<p>M&ouml;chten Sie den Newsletter empfangen um aktuelle Informationen zum Spiel geschehen und neuerungen erhalten?</p>
+		<p><input type="checkbox" name="msg[1]" id="msg1393" value="1393">Ja, ich m&ouml;chte den Newsletter erhalten</p>
+		<p><input type="checkbox" name="msg[1]" id="msg1393" value="1393">Nein, ich m&ouml;chte den Newsletter nicht erhalten </p>
+		
+		<input type="submit" name="submit4" value="speichern" />
+		
+	</div> <!-- close profilAll -->
 <?php
 }
 else{
